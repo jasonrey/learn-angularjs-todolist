6175f848dafacdaa2f9e13932f0a65975a9edfe5
Jason Rey <jasonrey@outlook.com>
2015-01-08 01:18:20 +0800

    Added router middleware to ensure data.json existence

--- api.js
+++ api.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+// Mandatory middleware for all router to go through to ensure that data.json file is there
+router.use(function(req, res, next) {
+    if (!fs.existsSync('./data.json')) {
+        fs.writeFileSync('./data.json', '[]');
+    }
+
+    next();
+});
+
 // For this sample, we use filesystem to read/store data to data.json
 // This can be easily changed to database
 router.get('/items', function(req, res, next) {
